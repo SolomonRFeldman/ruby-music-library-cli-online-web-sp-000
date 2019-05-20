30481f220ff22b52de79f14e704f8a26d93290d7
Solomon Feldman <solomonrfeldman@gmail.com>
2019-05-20 21:37:59 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -34,7 +34,7 @@ class MusicLibraryController
   def list_artists
     song_count = 0
     @library.sort { |song_a, song_b|
-      song_a.artist <=> song_b.artist
+      song_a.artist.name <=> song_b.artist.name
     }.each { |song|
       song_count += 1
       puts "#{song_count}. #{song.artist.name}" 
