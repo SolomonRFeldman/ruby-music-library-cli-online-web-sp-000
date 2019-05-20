a3c69eb6b9de7a9f46c27f53704a98ac3e1ffe30
Solomon Feldman <solomonrfeldman@gmail.com>
2019-05-20 20:26:45 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -6,7 +6,7 @@ module Concerns::Findable
   
   def find_or_create_by_name(name)
     #find_by_name(name) || create(name)
-    create(name) unless find_by_name(name)
+    (create(name)) unless find_by_name(name)
   end
   
 end
