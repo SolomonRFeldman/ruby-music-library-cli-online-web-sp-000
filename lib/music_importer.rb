9587f9ba4dede333a807220e307958657effb02b
Solomon Feldman <solomonrfeldman@gmail.com>
2019-05-20 21:07:25 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -10,7 +10,7 @@ class MusicImporter
   end
   
   def import
-    files.each { |filename| Song.create_from_filename(filename) }
+    files.collect { |filename| Song.create_from_filename(filename) }
   end
   
 end
