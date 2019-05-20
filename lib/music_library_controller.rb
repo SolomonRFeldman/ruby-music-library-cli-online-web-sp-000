de9fd2c7fe2e5146a17f090390645e6e59a9a226
Solomon Feldman <solomonrfeldman@gmail.com>
2019-05-20 21:20:17 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -22,7 +22,7 @@ class MusicLibraryController
   end
   
   def list_songs
-    @library.sort { |a, b|
+    @library.sort { |song_a, song_b| song_a.name <=> song_b.name
       .each { |song| puts "1. #{song.name} - #{song.artist.name} - #{song.genre.name}" }
   end
   
