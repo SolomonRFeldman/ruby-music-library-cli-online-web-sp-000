f7efbdc7991cb9088c6ff789ff8609e9bd2d9a31
Solomon Feldman <solomonrfeldman@gmail.com>
2019-05-20 21:00:28 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -14,6 +14,10 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    
+    gets command
+    while command != "exit"
+      puts "What would you like to do?"
+      gets command
+    end
   
 end
